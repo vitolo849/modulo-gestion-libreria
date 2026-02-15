e2809a7b16eb5ad92028d5ae6bba02cea9725ae5
AnthonyBrayanVivenes <35461970+AnthonyBVivenes@users.noreply.github.com>
2026-02-14 20:48:20 -0400

    Update main.py

--- main.py
+++ main.py
@@ -27,6 +27,11 @@ def main(page: ft.Page):
         accion = e.control.data
         if accion == "agregar_producto":
             content_area.content = ft.Text("Agregar nuevo producto", size=25)
+            content_area.content = txt_basico = ft.TextField(
+                label="Nombre",
+                hint_text="Nombre producto",
+                width=300,
+            )
         elif accion == "eliminar_producto":
             content_area.content = ft.Text("Eliminar producto", size=25)
         elif accion == "ver_productos":
