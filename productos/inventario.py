3dc2e3769282c80e1754adb055cbc3be6561de23
AnthonyBrayanVivenes <35461970+AnthonyBVivenes@users.noreply.github.com>
2026-02-14 22:11:48 -0400

    Update inventario.py

--- productos/inventario.py
+++ productos/inventario.py
@@ -1,34 +1,46 @@
 def view(content_area, ft):
+    # Datos de ejemplo
+    productos = [
+        ["Lhgfhf", "978-3-16-148410-0", "$1200", "15"],
+        ["Moudfgfdgdse gfgd", "978-0-13-110362-8", "$25", "50"],
+        ["gdf dfgdgd", "978-0-596-52068-7", "$45", "30"],
+        ["Mdfgdgonitor dfgd dfgdfgdg", "978-1-4919-1889-0", "$300", "10"],
+    ]
+    
+    filas = []
+    for p in productos:
+        filas.append(
+            ft.DataRow(
+                cells=[
+                    ft.DataCell(ft.Text(p[0])),
+                    ft.DataCell(ft.Text(p[1])),
+                    ft.DataCell(ft.Text(p[2])),
+                    ft.DataCell(ft.Text(p[3])),
+                ]
+            )
+        )
+    
+    # Crear la tabla
+    tabla = ft.DataTable(
+        columns=[
+            ft.DataColumn(ft.Text("Producto")),
+            ft.DataColumn(ft.Text("ISBN")),
+            ft.DataColumn(ft.Text("Precio")),
+            ft.DataColumn(ft.Text("Stock")),
+        ],
+        rows=filas,
+        border=ft.border.all(1, ft.Colors.BLACK),
+        border_radius=10,
+    )
     
     content_area.content = ft.Column([
-        ft.Text("Agregar nuevo producto", size=25),
-        ft.TextField(
-            label="Nombre",
-            hint_text="Nombre producto",
-            width=300,
+        ft.Text("INVENTARIO DE PRODUCTOS", size=30, weight=ft.FontWeight.BOLD),
+        
+        ft.Container(
+            content=ft.Column([tabla], scroll=ft.ScrollMode.AUTO),
+            height=400,
+            border=ft.border.all(1, ft.Colors.BLACK),
+            border_radius=5,
+            padding=10,
         ),
-        ft.TextField(
-            label="ISBN",
-            hint_text="ISBN",
-            width=300,
-        ),
-        ft.TextField(
-            label="Precio",
-            hint_text="Precio",
-            width=300,
-            keyboard_type=ft.KeyboardType.NUMBER,
-        ),
-        ft.TextField(
-            label="Stock mínimo",
-            hint_text="Stock mínimo",
-            width=300,
-            keyboard_type=ft.KeyboardType.NUMBER,
-        ),
-        ft.ElevatedButton("Guardar", on_click=lambda e: guardar_producto()),
     ], spacing=10)
-
-
-
-def guardar_producto():
-    #fdfdkfldkf
-    print("sds")
